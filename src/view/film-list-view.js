06160936283983c837c1024dff62bb129a2699b2
CodeforMood <strizhov.vladislav96@gmail.com>
2022-12-09 19:32:33 +0500

    Подключает bundle.js в index.html и создаёт модули с классами для генерации DOM-элементов

---
+++ src/view/film-list-view.js
@@ -0,0 +1,27 @@
+import {createElement} from '../render.js';
+
+function createFilmListTemplate() {
+  return (
+    `<section class="films-list">
+        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+     </section>`
+  );
+}
+
+export default class FilmListView {
+  getTemplate() {
+    return createFilmListTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
