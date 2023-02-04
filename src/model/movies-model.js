71d92834f36d113debca3ce63656b45871d2cb22
CodeforMood <strizhov.vladislav96@gmail.com>
2023-02-04 11:36:11 +0500

    Подправит моковые данные. Создаст модель комментариев, модель фильтров и исправит модель фильмов

--- src/model/movies-model.js
+++ src/model/movies-model.js
@@ -1,15 +1,27 @@
 import { mockMovies } from '../mock/data.js';
-import { mockComments } from '../mock/data.js';
+import Observable from '../framework/observable.js';
 
-export default class MoviesModel {
+export default class MoviesModel extends Observable {
   #moviesData = mockMovies;
-  #movieComments = mockComments;
+
 
   get moviesData() {
     return this.#moviesData;
   }
 
-  getComments(filmId) {
-    return this.moviesData.find((movieData) => movieData.id === filmId).comments.map((comment) => this.#movieComments.find((movieComment) => String(comment) === movieComment.id));
+  updateMovie(updateType, update) {
+    const index = this.#moviesData.findIndex((movie) => movie.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting movie');
+    }
+
+    this.#moviesData = [
+      ...this.#moviesData.slice(0, index),
+      update,
+      ...this.#moviesData.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
   }
 }
